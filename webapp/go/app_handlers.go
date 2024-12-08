bfc98fb8641ca3c8960d867ddf5b836aabeee482
mo124121 <101929016+mo124121@users.noreply.github.com>
2024-12-08 05:02:36 +0000

    最新の椅子位置をとる関数を切り出しScore: 8408 https://portal.isucon.net/contestant/benchmark_jobs/22315

--- webapp/go/app_handlers.go
+++ webapp/go/app_handlers.go
@@ -831,6 +831,16 @@ type appGetNearbyChairsResponseChair struct {
 	CurrentCoordinate Coordinate `json:"current_coordinate"`
 }
 
+func getChairLocation(ctx context.Context, tx *sqlx.Tx, chair Chair) (*ChairLocation, error) {
+	chairLocation := &ChairLocation{}
+	err := tx.GetContext(
+		ctx,
+		chairLocation,
+		`SELECT * FROM chair_locations WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1`,
+		chair.ID,
+	)
+	return chairLocation, err
+}
 func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	latStr := r.URL.Query().Get("latitude")
@@ -912,13 +922,8 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// 最新の位置情報を取得
-		chairLocation := &ChairLocation{}
-		err = tx.GetContext(
-			ctx,
-			chairLocation,
-			`SELECT * FROM chair_locations WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1`,
-			chair.ID,
-		)
+		var chairLocation *ChairLocation
+		chairLocation, err = getChairLocation(ctx, tx, chair)
 		if err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
 				continue
